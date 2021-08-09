e1eb1cb85028b8f990d76598d8285684a63bf066
Russell Covington <RCovington@users.noreply.github.com>
2021-08-09 17:14:53 -0600

    adding files.

---
+++ precache-manifest.a4adffbac62a31e4f05e20d9cf9e9cb4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41cc99cec115e6ed58083d5c79502e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "332c75fb275c371b9f80",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "fdc31ada3aa6f4677e3d",
+    "url": "/static/js/2.d28d9970.chunk.js"
+  },
+  {
+    "revision": "a5db693410fed6a67f010a07b976fd8f",
+    "url": "/static/js/2.d28d9970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "332c75fb275c371b9f80",
+    "url": "/static/js/main.0933bf4d.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  },
+  {
+    "revision": "9a77dfe7c322585c6d4121157079cae5",
+    "url": "/static/media/rpsls-all.9a77dfe7.png"
+  },
+  {
+    "revision": "bfb0e42a08ac7aa9d4bc0244f01c5b6c",
+    "url": "/static/media/rpsls-lizard.bfb0e42a.png"
+  },
+  {
+    "revision": "7080a7e91108614cf31fc961af49ef60",
+    "url": "/static/media/rpsls-paper.7080a7e9.png"
+  },
+  {
+    "revision": "ca15151c9058906bbc3583ab012c8ccd",
+    "url": "/static/media/rpsls-rock.ca15151c.png"
+  },
+  {
+    "revision": "f60b53b6e53360016eb1ef3550d28214",
+    "url": "/static/media/rpsls-scissors.f60b53b6.png"
+  },
+  {
+    "revision": "280e80eaa79f15c0d2971d0453abd41c",
+    "url": "/static/media/rpsls-spock.280e80ea.png"
+  },
+  {
+    "revision": "1cfa24a5e82c665b700f38d9bdf04195",
+    "url": "/static/media/thumbs_up.1cfa24a5.png"
+  },
+  {
+    "revision": "6db44b04476f1385864179e398b16fa0",
+    "url": "/static/media/victory.6db44b04.png"
+  }
+]);
